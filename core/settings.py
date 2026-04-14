59de214c8da3e455b092ce10ca293e4bf7792c30
Danieleduardo2 <dbocachica@unimagdalena.edu.co>
2026-04-13 19:25:11 -0500

    Configurando variables

--- core/settings.py
+++ core/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kwiw^0qu+i815j%i)@&x-1nd^s!-a$#+v&xypq3j0!7)3r6q(='
-
+SECRET_KEY = os.getenv("SECRET_KEY", "dev-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
